f6f4f1e832f1a56df4fa0f51a3350e3d7b980a04
mkielczewski <mkielczewski@kpmg.pl>
2019-06-18 16:11:26 +0200

    rules

---
+++ ATeam/A_Team/Rules.cs
@@ -0,0 +1,49 @@
+﻿using A_Team.ATeam;
+using A_Team.Data;
+using A_Team.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A_Team
+{
+    internal class JiraInteractor : IJiraInteractor
+    {
+        public List<JiraItem> GetItems()
+        {
+            return new List<JiraItem>();
+        }
+
+        public bool UpdateItems(List<JiraItem> items)
+        {
+            return true;
+        }
+    }
+    public class Rules
+    {
+        private static readonly ContactsReader _cr = new ContactsReader();
+        private static readonly IJiraInteractor _jiraInteractor;
+
+        public static void RunRules()
+        {
+            var contacts = _cr.ReadContacts("C:/Users/mkielczewski/Desktop/Copy of Gdansk Hackathon - Jira Country contacts.csv ");
+            var jiraItemsToSend = _jiraInteractor.GetItems().Where(i => i.status == JiraStatusEnum.ToDo);
+            var date = DateTime.Now;
+            string quarter;
+            if (date.Month <= 3)
+                quarter = $"First Quarter {date.Year}";
+            else if (date.Month <= 6)
+                quarter = $"Second Quarter {date.Year}";
+            else if (date.Month <= 9)
+                quarter = $"Third Quarter {date.Year}";
+            else
+                quarter = $"Fourth Quarter {date.Year}";
+
+            foreach (var jiraItem in jiraItemsToSend)
+            {
+                // todo
+            }
+
+        }
+    }
+}

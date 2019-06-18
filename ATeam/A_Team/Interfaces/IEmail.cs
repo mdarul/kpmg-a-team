a8cb001c398b41f23a9f1f2a52dbe753f7ed7b16
Jaby ElHaramein, Sami <sjabyelharamein@kpmg.com>
2019-06-18 16:04:00 +0200

    Email structure

--- ATeam/A_Team/Interfaces/IEmail.cs
+++ ATeam/A_Team/Interfaces/IEmail.cs
@@ -7,5 +7,16 @@ namespace A_Team.Interfaces
 {
     interface IEmail
     {
+        string EmailSubject { get; set; }
+        string EmailBody { get; set; }
+        string EmailTo { get; set; }
+        string EmailFrom { get; set; }
+        string Country { get; set; }
+
+        string CC { get; set; }
+        string BCC { get; set; }
+        string ReplyTo { get; set; }
+
+        string SMTPServer { get; set; }
     }
 }
--- ATeam/A_Team/Interfaces/IEmailInteractor.cs
+++ ATeam/A_Team/Interfaces/IEmailInteractor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace A_Team.Interfaces
@@ -9,4 +10,50 @@ namespace A_Team.Interfaces
     {
         bool SendMails(List<IEmail> mails);
     }
+    class Email : IEmailInteractor
+    {
+        public bool SendMails(List<IEmail> mails)
+        {
+            foreach (var emailTemplate in mails)
+            {
+                var oMail = new MailMessage();
+                string EmailBody = "";
+
+                foreach (var address in emailTemplate.EmailTo.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    if (!string.IsNullOrEmpty(Convert.ToString(address)))
+                    {
+                        oMail.To.Add(address);
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(emailTemplate.ReplyTo))
+                {
+                    oMail.ReplyToList.Add(emailTemplate.ReplyTo.Replace(";", ","));
+                }
+
+                if (!string.IsNullOrEmpty(emailTemplate.EmailFrom))
+                {
+                    MailAddress fromAddress = new MailAddress(emailTemplate.EmailFrom);
+                    oMail.From = fromAddress;
+                }
+                EmailBody += emailTemplate.EmailBody;
+
+                using (var oClient = new SmtpClient())
+                {
+                    oClient.Port = 25;
+                    oClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    oClient.UseDefaultCredentials = false;
+                    oClient.Host = emailTemplate.SMTPServer;
+                    oMail.IsBodyHtml = true;
+                    oMail.Subject = emailTemplate.EmailSubject;
+                    oMail.Body = EmailBody;
+                    oClient.Send(oMail);
+                    oMail.Dispose();
+                }
+            }
+            return true;
+        }
+    }
+   
 }

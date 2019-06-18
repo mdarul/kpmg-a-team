f19fc42189cb25bbd843cb5c31f21015874b4edf
Jaby ElHaramein, Sami <sjabyelharamein@kpmg.com>
2019-06-18 15:43:46 +0200

    DisableHangfire

---
+++ ATeam/A_Team/HangFireAuthorizationFilter.cs
@@ -0,0 +1,20 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A_Team
+{
+    public class HangFireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            //can add some more logic here...
+            //return HttpContext.Current.User.Identity.IsAuthenticated;
+            //Hardcoded
+            return true;
+        }
+    }
+}
